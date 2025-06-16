745ebbd5bd91c78ef3d6e3a94f4d24e92554b9ed
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2025-06-16 12:15:55 +0000

    tweaks

--- ETG-Token/smart-contract/hardhat.config.js
+++ ETG-Token/smart-contract/hardhat.config.js
@@ -4,13 +4,9 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.28",
   networks: {
-    ganache: {
-      url: "http://127.0.0.1:8545", // Default RPC URL for Ganache
-      // chainId: 1337,               // Default Chain ID for Ganache
-      accounts: [
-        "0xd6020b890282ca4c13974ac73923f3dcfa5b8cc4593ee78bfdd6e74caa23a449",
-        // Add more private keys if needed
-      ]
+    sepolia: {
+      url: process.env.INFURA_URL, // Default RPC URL for Ganache
+      accounts: [`0x`+process.env.PRIVATE_KEY]
     },
   }
 };
